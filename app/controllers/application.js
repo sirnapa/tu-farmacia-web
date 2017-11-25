61c52ae748472a261adc62059e5ca47803c8e61d
Nelson Paez <nelpa90@gmail.com>
2017-11-25 14:56:57 -0300

    - Container vs. Fullscreen en Controller de Application - La tabla ya no está dentro del card-body en los index

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+
   hideMenuRoutes: ['login'],
   showMenu: Ember.computed('currentRouteName', function(){
       return this.get('hideMenuRoutes').indexOf(this.get('currentRouteName')) === -1;
+  }),
+
+  fullScreenRoutes: ['index'],
+  showInContainer: Ember.computed('currentRouteName', function(){
+      return this.get('fullScreenRoutes').indexOf(this.get('currentRouteName')) === -1;
   }),
 
   actions: {
